62a1e28264141672f8b846ed9435f8dd66dd2117
yunji-1ee <yunji011219@gmail.com>
2024-05-11 23:02:04 +0900

    DS43

--- Lab10/DS043.cpp
+++ Lab10/DS043.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <stdexcept>
-#include <cctype>ㄴ
+#include <stack>
 using namespace std;
 
 class MyLinkedStack {
 private:
     struct Node {
-        int data;
+        char data;
         Node* next;
-        Node(int data) : data(data), next(nullptr) {}
+        Node(char data) : data(data), next(nullptr) {}
     };
     Node* stacktop;
     int nodeCnt;
@@ -21,80 +19,66 @@ public:
         while (!isEmpty()) pop();
     }
 
-    bool isEmpty() const { return stacktop == NULL; }
-    void push(int data) {
+    bool isEmpty() const { return stacktop == nullptr; }
+    void push(char data) {
         Node* newNode = new Node(data);
         newNode->next = stacktop;
         stacktop = newNode;
         nodeCnt++;
     }
-    int pop() {
-        int data = stacktop->data;
+    char pop() {
+        if (isEmpty()) throw runtime_error("Stack underflow");
+        char data = stacktop->data;
         Node* temp = stacktop;
         stacktop = stacktop->next;
         delete temp;
         nodeCnt--;
         return data;
     }
-
-    int getNodeCnt() const { return nodeCnt; }
 };
 
-int evaluateExpression(const string& expression) {
-    MyLinkedStack stack;
-    stringstream ss(expression);
-    string token;
-    int result = 0;
-
-    while (ss >> token) {
-        if (token == "=") {
-            if (stack.getNodeCnt() == 1) {
-                result = stack.pop();
-                return result;  
-            } else {
-                cout << expression << " => Too many operands !!!" << endl;
-                return -1;
+void processInput(const string& input) {
+    MyLinkedStack leftStack, rightStack;
+    
+    for (size_t i = 0; i < input.length(); ++i) {
+        char ch = input[i];
+        if (ch == '<') {
+            if (!leftStack.isEmpty()) {
+                char moved = leftStack.pop();
+                rightStack.push(moved);
             }
-
-
-        } else if (token[0] == '#' && isdigit(token[1])) {
-            stack.push(stoi(token.substr(1)));
-
-        } else if (token == "+" || token == "-" || token == "*" || token == "/")
-         {//연산식
-            if (stack.getNodeCnt() < 2) {
-                return -1;
+        } else if (ch == '>') {
+            if (!rightStack.isEmpty()) {
+                char moved = rightStack.pop();
+                leftStack.push(moved);
             }
-            int right = stack.pop();
-            int left = stack.pop();
-            switch (token[0]) {
-                case '+': stack.push(left + right); break;
-                case '-': stack.push(left - right); break;
-                case '*': stack.push(left * right); break;
-                case '/':
-                    if (right == 0) {
-                        cout << expression << " => Division by 0 !!!" << endl;
-                        return -1;
-                    }
-                    stack.push(left / right);
-                    break;
-                default:
-                    cout << expression << " => Expression error !!!" << endl;
-                    return -1;
-            }
+        } else {
+            leftStack.push(ch);
         }
     }
 
-    cout << expression << " => Expression error !!!" << endl;
-    return -1;
+    // 오른쪽 스택에서 왼쪽스텍으로 reverse
+    while (!leftStack.isEmpty()) {
+        rightStack.push(leftStack.pop());
+    }
+
+
+    string result;
+    while (!rightStack.isEmpty()) {
+        result += rightStack.pop();
+    }
+
+    cout << "=> " << result << endl;
 }
 
 int main() {
-    string expression;
-    getline(cin, expression);
-    int result = evaluateExpression(expression);
-    if (result != -1) {
-        cout << expression << result << endl;
+    string input;
+
+    while (getline(cin, input)) {
+        if (input == "q") break;
+        processInput(input);
     }
+
+ 
     return 0;
 }
